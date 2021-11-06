62ebbb611446ce129f707353b8390ebd2a02e950
omerberk123 <omerberkilhan@gmail.com>
2021-11-06 13:56:32 +0300

    06112021-1356

---
+++ 01_python_giris/0118_degiskenler.py
@@ -0,0 +1,39 @@
+#region degisken_isimlendirme_kurallari
+"""
+Değişken İsimlendirme Kuralları
+1- harf veya alt çizgi ile başlamalıdır
+2- rakam ile başlayamaz
+3- ilk harf dışındakiler, harf, sayı, alt çizgi olabilir
+4- alt çizgi dışında alfa sayısal karakterlerimiz (%, #, $...) kullanılamaz
+5- case sensitive
+6- anahtar kelimeler if, pass, while, def bunlar kullanılamaz
+7- türkçe karakter kullanmamaya özen gösterelim
+8-deve notasyonu örnegi azizHoca
+9-Underscore örnegi aziz_hoca_ile_python_ögreniyorum
+"""
+#endregion
+
+"""
+name = "John"
+surname = "Mercedes"
+age = 45
+print(name)
+print(surname)
+print(name, surname, age)
+
+
+okulNo = "1065"
+ad = "Ender"
+soyad = "Baris"
+print(ad, soyad, okulNo)
+"""
+
+_1nciOgrenci = "Buse"
+print(_1nciOgrenci)
+
+plaka35 = "izmir"
+print(plaka35)
+"""
+import keyword
+print(keyword.kwlist) 
+"""
---
+++ 01_python_giris/0119_concat.py
@@ -0,0 +1,19 @@
+okulNo = "1065"
+ad = "Ender"
+soyad = "Bartu"
+#print("Öğrenci Adı", ad,"Soyadı", soyad,"Okul Numarası", okulNo)
+
+
+print("Öğrenci Adı ve Soyadı " + ad + " " + soyad +" olan. Öğrencimizin okul numarası " + okulNo)
+
+# Ayakkabı numaram 41. Favori markam adidas.
+
+no = "41"
+marka = "adidas"
+
+print("Ayakkabı numaram " + no + " Favori markam " + marka + ".")
+
+ad= "Berk"
+kahve= "Türk kahvesi"
+
+print("Merhaba ben " + ad + ". En sevdiğim kahve " + kahve + ".")
